35339acd92607738510d5af02d8ebf2d62b72e6d
Alphan Gunaydin <ialphan@ialphan.com>
2012-10-06 19:39:08 -0400

    Clean up. :)

--- assets/js/signaway.js
+++ assets/js/signaway.js
@@ -1,43 +1,6 @@
 $(function(){
-  console.log('hello signaway');
-  
-  // update this apigee post url.
-  var apigeePostURL = null
-  
-  $('#signup').on('click', function(){
-    
-    /* Tony: Do error check / validation here.
-    url is here http://bassistance.de/jquery-plugins/jquery-plugin-validation/
-      Ex.
-      - Is the email address field an email address
-      - first/last name length, can it include numbers, etc
-      - event code is it numbers only, length check, any character allowed? 
-    stuff like this...
-      
-      
-      if all validation is done do a post
-      else show error message based on failed validation
-    */
-    
-     
-//  simple
-    $.post(apigeePostURL, $('#signaway').serialize());
-
-
-// more advanced    
-    // $.post(url, $('#signaway').serialize(),function(data, textStatus, jqXHR){  
-    //   console.log('data');
-    // }).error(function(){
-    //   /*
-    //     TODO Do error checking.
-    //   */
-    //   alert('Internal Server Error! :(');
-    // });
- 
- 
- /* success  make sure you show a message saying you are signed up */
- /* error: internal error, etc */
- 
+  $('#signup').on('click', function(){('https://api.usergrid.com/signaway/signaway/users', JSON.stringify({firstName:$('#firstName').val(), lastName:$('#lastName').val(), eventCode:$('#eventCode').val()}));
+    $('body').html('<div id="confirmed"><h1>Thanks!<br>You are now signed up.</h1><br><img alt="SignAway" id="signAwayLogo" src="https://raw.github.com/signaway/signaway/gh-pages/assets/js/logo.png" width="300" height="300"></div>')
     return false;
   });
 });
