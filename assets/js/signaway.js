abb747e3dc6d8516670c414a498066fdab3b7e6d
bkapicka <bkapicka@gmail.com>
2012-10-06 21:08:03 -0300

    Update assets/js/signaway.js

--- assets/js/signaway.js
+++ assets/js/signaway.js
@@ -1,6 +1,6 @@
 $(function(){
   $('#signup').on('click', function(){('https://api.usergrid.com/signaway/signaway/users', JSON.stringify({firstName:$('#firstName').val(), lastName:$('#lastName').val(), eventCode:$('#eventCode').val()}));
-    $('body').html('<div id="confirmed"><h1>Thanks!<br>You are now signed up.</h1><br><img alt="SignAway" id="signAwayLogo" src="https://raw.github.com/signaway/signaway/gh-pages/assets/js/logo.png" width="300" height="300"></div>')
+    $('body').html('<div id="confirmed"><h1>Thanks!<br>You are now signed up.</h1><br><img alt="SignAway" id="signAwayLogo" src="https://raw.github.com/signaway/signaway/gh-pages/assets/img/logo.png" width="300" height="300"></div>')
     return false;
   });
 });
